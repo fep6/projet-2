d1e1303fa7e61106cdc7b4879f9b4e165721dbf4
fep <franck.en.planck@gmail.com>
2020-10-13 10:25:41 +0200

    creation de la classe main AnalyticsCounter() : renommage i en lineNumberSymptomsTxt (?)

--- Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master/Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master/Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,23 +1,25 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
 	
-	public static void main(String args[]) throws Exception {
+	public AnalyticsCounter() throws IOException  {
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
+		int lineNumberSymptomsTxt = 0;	// set i to 0 (lineNumber of symptoms.txt?)
 		int headCount = 0;	// counts headaches
 		while (line != null) {
-			i++;	// increment i
+			lineNumberSymptomsTxt++;	// increment i (lineNumber?)
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
 				headCount++;
