87c1e9386287c9a72892584988345a6a2f490674
fep <franck.en.planck@gmail.com>
2020-10-17 13:12:23 +0200

    refection algo de doClassification()

--- Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master/Project02Eclipse/src/com/treatment/Classification.java
+++ Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master/Project02Eclipse/src/com/treatment/Classification.java
@@ -10,43 +10,48 @@ public class Classification{
 	/**
 	 * pointer values ascending
 	 */
-	private int PointerOfHashMap;
+	private int PointerHashMap;
 	/**
 	 *  pointer values descending
 	 */
-	private int newPointerOfHashMap;
+	private int newPointerHashMap;
 	/**
 	 * pointer of the letter
 	 */
-	private int letterPointer;
+	// private int letterPointer;
 	/**
 	 * Comparative first letter between value n and n+1 and if equal, the following letters
 	 * @param symptomsTxt
 	 * Coming from Input
 	 */
 	void doClassification(HashMap<Integer, String> symptomsTxt) {
-		PointerOfHashMap =1;
+		PointerHashMap =1;
+		
 		// look for the unicode of the first (1) letter in the value of the Hashmap
-		codeLetters(symptomsTxt,PointerOfHashMap,1);
-		// While the pointer is under the size of the HashMap
-		while (PointerOfHashMap<=symptomsTxt.size()) {
-			for (newPointerOfHashMap=PointerOfHashMap;newPointerOfHashMap>=0;newPointerOfHashMap--) {
-				if (newPointerOfHashMap>newPointerOfHashMap-1) {
+		// codeLetters(symptomsTxt,PointerOfHashMap,1);
+		
+		// Pointing each following couple <key,value> in the  HashMap
+		while (PointerHashMap<symptomsTxt.size()) {
+			//newPointerHashMap=2;
+			
+			// Research of the minimum number of character between the 2 values to compare
+			int iteration = Math.min(symptomsTxt.get(PointerHashMap).length(),symptomsTxt.get(PointerHashMap+1).length());
+			
+			// Compare values, by couple of letters in the same rank 
+			for (int pointerLetter=1;pointerLetter<=iteration;pointerLetter++) {
+				
+				// Construction of 
+				codeLetters(symptomsTxt,PointerHashMap,pointerLetter);
+				
+				if (codeLetterValueSymptomsTxt.get(pointerLetter)<codeLetterValueSymptomsTxt.get(pointerLetter+1)){
 					break;
 				}
-				// At the same rank, if the same rank letters between values are the same, 
-				// then compare other couple letters at the next same rank: 
-				else if ((newPointerOfHashMap==newPointerOfHashMap-1)){
-					letterPointer = newPointerOfHashMap;
-					// as long as all couple letters at the same rank are the same and not all of them are each compared for each
-					// while ()
-				}
 				else {
-				// Exchange
+				// Exchange 
 				// Comparative descending  
-				}
-				PointerOfHashMap++;			
+				}			
 			}
+			PointerHashMap++;
 		}
 	}
 	/**
@@ -59,20 +64,20 @@ public class Classification{
 	 * rank of the letter targeted in the value of the hashMap<key,value>
 	 * (-1 because HashMap key begin at 1)
 	 */
-	void codeLetters(HashMap<Integer, String> symptomsTxt, int pointer , int rankLetter) {
-		for (pointer=PointerOfHashMap; pointer <= symptomsTxt.size(); pointer++) {
+	void codeLetters(HashMap<Integer, String> symptomsTxt, int pointer , int pointerLetter) {
+		for (pointer=PointerHashMap; pointer <= symptomsTxt.size(); pointer++) {
 			// reserch Unicode of the char choosen (/rank) in the value of HashMap
-			int buffer= (int)((symptomsTxt.get(pointer).charAt(rankLetter - 1)));
+			int buffer= (int)((symptomsTxt.get(pointer).charAt(pointerLetter - 1)));
 			// Put the code in a new Hashcode to be compare
 			codeLetterValueSymptomsTxt.put(pointer, buffer);
 		}
-		System.out.println("CodeLetters value at the " + rankLetter + " rank are: ");
+		System.out.println("CodeLetters value at the " + pointerLetter + " rank are: ");
 		System.out.println(codeLetterValueSymptomsTxt.values());
 		// System.out.println(codeLetterValueSymptomsTxt.keySet());
-		System.out.println("Nombre de caractères dans value de symptomsTxt à la valeur "+ PointerOfHashMap + " :");
-		System.out.println((symptomsTxt.get(PointerOfHashMap)).length());
+		System.out.println("Nombre de caractères dans value de symptomsTxt à la valeur "+ PointerHashMap + " :");
+		System.out.println((symptomsTxt.get(PointerHashMap)).length());
 		System.out.println("Valeur minimale entre le nombre de caractères entre pointeur n et pointeur n+1:");
-		System.out.println(Math.min(symptomsTxt.get(PointerOfHashMap).length(),symptomsTxt.get(PointerOfHashMap+1).length()));
+		System.out.println(Math.min(symptomsTxt.get(PointerHashMap).length(),symptomsTxt.get(PointerHashMap+1).length()));
 	}
 	
 	HashMap<Integer, String> getClassification(){
