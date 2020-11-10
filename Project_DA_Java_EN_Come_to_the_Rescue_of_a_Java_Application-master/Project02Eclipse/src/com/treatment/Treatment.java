a65ae260b1917d67f871a813ce55127ca7ff2157
fep <franck.en.planck@gmail.com>
2020-11-10 14:33:28 +0100

    Suppression classement

--- Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master/Project02Eclipse/src/com/treatment/Treatment.java
+++ Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master/Project02Eclipse/src/com/treatment/Treatment.java
@@ -18,9 +18,9 @@ public class Treatment {
 	
 	public void doTreatment(ArrayList<String> symptomsTxt ){
 		// sort the ArrayList in ascending order
-		symptomsTxt.sort(Comparator.naturalOrder());
-		System.out.println("Fichier classé= " + symptomsTxt);  
-		System.out.println("______________________________");
+		// symptomsTxt.sort(Comparator.naturalOrder());
+		// System.out.println("Fichier classé= " + symptomsTxt);  
+		// System.out.println("______________________________");
 		
 		
 		// Integration of symptomTxt in a TreeMap
