160af3da04f08ddee13cdae82befe9fb17200b8a
fep <franck.en.planck@gmail.com>
2020-11-12 14:05:01 +0100

    mef

--- Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master/Project02Eclipse/src/com/principal/Main.java
+++ Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master/Project02Eclipse/src/com/principal/Main.java
@@ -3,10 +3,6 @@ package com.principal;
 
 import java.io.IOException;
 
-// 02/11/20 14:14 NewBranch treeSet
-
-
-
 public class Main {
 
 	public static void main(String[] args) throws IOException  {
